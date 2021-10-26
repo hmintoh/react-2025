aad4bad61822ccb498578da2fffd44ab7ff12b60
hmintoh <hmintoh@gmail.com>
2021-10-26 17:21:12 +0800

    feat(pages/index): set up skeletal login/logout flow

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,38 +2,29 @@ import type { NextPage } from 'next';
 import { useAuth } from 'lib/auth';
 import Head from 'next/head';
 
-import { Button, Text, Heading, Container } from '@chakra-ui/react';
-import styles from 'styles/Home.module.css';
+import { Button, Flex } from '@chakra-ui/react';
 
 const Home: NextPage = () => {
   const auth = useAuth();
 
   return (
-    <div className={styles.container}>
+    <Flex
+      as="main"
+      direction="column"
+      align="center"
+      justify="center"
+      height="100vh"
+    >
       <Head>
         <title>Fast Feedback</title>
       </Head>
 
-      <main className={styles.main}>
-        <Heading as="h1" size="4xl">
-          Welcome to Fast Feedback{' '}
-        </Heading>
-
-        <Container>
-          <Button onClick={() => auth.signInWithGithub()}>sign in</Button>
-
-          {auth.user && (
-            <>
-              <Text fontSize="xl">{auth.user.name}</Text>
-              <Text fontSize="xl">{auth.user.email}</Text>
-              <Button onClick={() => auth.signout()}>sign out</Button>
-            </>
-          )}
-        </Container>
-      </main>
-
-      <footer className={styles.footer}>footer</footer>
-    </div>
+      {auth.user ? (
+        <Button onClick={() => auth.signout()}>Sign out</Button>
+      ) : (
+        <Button onClick={() => auth.signInWithGithub()}>Sign in</Button>
+      )}
+    </Flex>
   );
 };
 
