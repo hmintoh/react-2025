be5fc6d6dbba08c331e797b5073e14ecfeaf05e3
hmintoh <hmintoh@gmail.com>
2021-11-22 20:32:14 +0800

    fix(lib/firebase): initialise firebase client only when length = undefined

--- lib/firebase.tsx
+++ lib/firebase.tsx
@@ -1,9 +1,13 @@
-import { initializeApp } from 'firebase/app';
+import * as fb from 'firebase/app';
 
-const firebase = initializeApp({
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-});
+let firebase: fb.FirebaseApp;
+
+if (!fb.getApps.length) {
+  firebase = fb.initializeApp({
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  });
+}
 
 export { firebase };
